cfbf3c1e267a0eb3078f0ec86e9a029e4f21a779
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-03-02 11:13:56 -0800

    Finished first part of 02 challenge

--- 02_slice/main.go
+++ 02_slice/main.go
@@ -7,5 +7,9 @@ func main() {
 	var slicey []int
 	fmt.Println("This is what we start with:")
 	fmt.Println(slicey, len(slicey), cap(slicey))
-
+	fmt.Println("---------------------------")
+	for i := 1; i < 11; i++ {
+		slicey = append(slicey, i)
+		fmt.Println("val: ", i, "size of slice: ", len(slicey), "capacity of slice: ", cap(slicey))
+	}
 }
